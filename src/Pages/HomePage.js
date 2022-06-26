205f43c75c19ebf7ef05e8de87fe930413b4d72d
Ethan Freeman <ethanfree23@gmail.com>
2022-06-26 17:25:26 -0500

    home page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,11 +6,18 @@ import WhatWeDo from '../Components/WhatWeDo.js'
 function HomePage() {
     return (
         <div>
-            <div className="bg-hero-image bg-cover h-[800px] -mt-6" >
-                <p className="text-white font-extrabold">hello world</p>
+            <div className=" bg-hero-image bg-cover h-[800px] -mt-6" >
+                <div className="border border-[#ffffff00]">
+                    <div className="border border-black my-24 mx-44 p-12 pt-16" >
+                        <h2 className="text-white font-extrabold text-6xl text-center">Tee's Home Services</h2>
+                        <h6 className="text-white text-3xl text-center">Plumbing, Remodeling, & Electrical</h6>
+                    </div>
+                </div>
             </div>
-            <WhyChooseUse />
-            <WhatWeDo />
+            <div>
+                <WhyChooseUse />
+                <WhatWeDo />
+            </div>
         </div>
     )
 }
