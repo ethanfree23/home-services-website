8e7b32ac3202f9328e4803fb2896a2cb6dfb4bcd
Ethan Freeman <ethanfree23@gmail.com>
2022-06-26 22:26:51 -0500

    home page hero section edit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -32,22 +32,22 @@ function NavBar() {
                                     )}
                                 </Disclosure.Button>
                             </div>
-                            {/* <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex-shrink-0 flex items-center">
                                     <img
                                         className="block lg:hidden h-8 w-auto"
                                         src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                                         alt="Workflow"
-                                    /> */}
+                                    />
 
-                            {/* Navbar image */}
+                                    {/* Navbar image */}
 
-                            {/* <img
+                                    {/* <img
                                         className="hidden lg:block h-8 w-auto"
                                         src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
                                         alt="Workflow"
                                     /> */}
-                            {/* </div>
+                                </div>
                                 <div className="hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
@@ -65,7 +65,7 @@ function NavBar() {
                                         ))}
                                     </div>
                                 </div>
-                            </div> */}
+                            </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 <button
                                     type="button"
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -7,8 +7,8 @@ function HomePage() {
     return (
         <div>
             <div className=" bg-hero-image bg-cover h-[800px] -mt-6" >
-                <div className="border border-[#ffffff00]">
-                    <div className="border border-[#dce0e0] my-36 mx-44 p-12 pt-16" >
+                <div className="border border-[#ffffff00] ">
+                    <div className="border border-[#dce0e0] bg-[#f9f9f920] my-36 mx-44 p-12 pt-16" >
                         <h2 className="text-white font-extrabold text-8xl text-center">Tee's Home Services</h2>
                         <h6 className="text-white text-5xl text-center mt-4">Plumbing, Remodeling, & Electrical</h6>
                     </div>
